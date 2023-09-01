8f14543b638688394ae1d2bf88ac4f442d7d2087
uvaisantilia <uvais@antiliasolutions.com>
2023-09-01 17:35:29 +0530

    environment

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,5 +1,6 @@
 export const environment = {
   production: false,
-  apiUrl: 'http://localhost:60978/api/',
+  apiUrl: 'http://localhost:5213/api/',
+  apiUrl1 : `http://16.171.114.35:83/api/`,
   debug: true
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,7 @@
 export const environment = {
   production: true,
   apiUrl: 'https://my-production-api.com/api',
+  apiUrl1 : `http://16.171.114.35:83/api/`,
   debug: false
 };
 
